26e71e555a19d85365989340235f4a8b296d1b49
MahFelix <marcelo12543@hotmail.com>
2023-05-03 19:49:18 -0300

    Initialize project using Create React App

---
+++ src/globalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle  } from "styled-components";
+
+//  Ou export default createglobalStyle solo
+const Globalstyle = createGlobalStyle `
+  *
+{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sem serifa;
+}
+`;
+
+export default Globalstyle
